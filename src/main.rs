65099a4ca41af2519181f76e23c13202c9a4cfca
Abhemanyu Sarin <abhemanyus@gmail.com>
2024-09-06 17:42:25 +0530

    working terminal with stdin, stdout and stderr

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{
+    extract::Query,
     response::sse::{Event, KeepAlive, Sse},
     routing::get,
     Router,
@@ -19,23 +20,39 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn run() -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+async fn run(Query(cmd): Query<Cmd>) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
     let mut command = Command::new("bash");
     command.arg("-c");
-    command.arg("while true\n do\n echo \"Hi!\"\n sleep 1\n done");
+    command.arg(cmd.command);
     command.stdin(Stdio::null());
-    command.stderr(Stdio::inherit());
+    command.stderr(Stdio::piped());
     command.stdout(Stdio::piped());
     let mut child = command.spawn().unwrap();
     let output = child.stdout.take().unwrap();
-    let stream = ReaderStream::new(output)
+    let error = child.stderr.take().unwrap();
+    let stream_out = ReaderStream::new(output)
         .filter_map(|data| {
             data.ok().and_then(|data| {
                 String::from_utf8(data.to_vec())
                     .ok()
-                    .and_then(|data| Event::default().json_data(data).ok())
+                    .and_then(|data| Event::default().event("stdout").json_data(data).ok())
             })
         })
         .map(Ok);
+    let stream_err = ReaderStream::new(error)
+        .filter_map(|data| {
+            data.ok().and_then(|data| {
+                String::from_utf8(data.to_vec())
+                    .ok()
+                    .and_then(|data| Event::default().event("stderr").json_data(data).ok())
+            })
+        })
+        .map(Ok);
+    let stream = stream_out.merge(stream_err);
     Sse::new(stream).keep_alive(KeepAlive::default())
+}
+
+#[derive(serde::Deserialize)]
+struct Cmd {
+    command: String,
 }
